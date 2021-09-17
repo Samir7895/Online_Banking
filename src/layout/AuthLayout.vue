7fa985816a0da805fd231a4899e519c8e6b203a1
Samir7895 <samir@think.az>
2021-09-17 12:20:01 +0400

    Create page structure and add layout

---
+++ src/layout/AuthLayout.vue
@@ -0,0 +1,18 @@
+<template>
+
+  <div class="container with-nav">
+    <div class="card">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AuthLayout"
+}
+</script>
+
+<style scoped>
+
+</style>
